64070a81e5b5d3bf0b9652b23c9765f99e59ee9f
Adirv5 <Modernfamiliy11yad@outlook.com>
2026-02-06 15:43:27 -0600

    Parche: configurar respaldo de OneDrive a disco externo

--- copiasseg.ps1
+++ copiasseg.ps1
@@ -1,6 +1,6 @@
-# Ruta origen y destino
-$origen = "C:\Datos"
-$destino = "D:\Backups"
+# Ruta origen (OneDrive Local) y destino (Disco Externo)
+$origen = "C:\Users\ioriy\OneDrive"
+$destino = "R:\BACKUPS\OneDrive"
 
 # Crear carpeta con fecha
 $fecha = Get-Date -Format "yyyy-MM-dd_HH-mm"
